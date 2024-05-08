dc6f9dc1897b358d829cd495fad7b45d1ed5bfbf
yuri skoda <gwestonem@gmail.com>
2024-05-08 15:56:54 +0300

    completed frontend

--- main/lib/http/http.c
+++ main/lib/http/http.c
@@ -1,4 +1,5 @@
 #include "http.h"
+#include "frontend.h"
 #include "event.h"
 
 // Asynchronous response data structure
@@ -12,43 +13,42 @@ struct async_resp_arg
 esp_err_t get_handler(httpd_req_t *req)
 {
     /* Send a simple response */
-    const char resp[] = "URI GET Response";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send(req, GLOBAL_FRONTEND, HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
 }
 
 /* Our URI handler function to be called during POST /uri request */
-esp_err_t post_handler(httpd_req_t *req)
-{
-    /* Destination buffer for content of HTTP POST request.
-     * httpd_req_recv() accepts char* only, but content could
-     * as well be any binary data (needs type casting).
-     * In case of string data, null termination will be absent, and
-     * content length would give length of string */
-    char content[100];
-
-    /* Truncate if content length larger than the buffer */
-    size_t recv_size = MIN(req->content_len, sizeof(content));
-
-    int ret = httpd_req_recv(req, content, recv_size);
-    if (ret <= 0) {  /* 0 return value indicates connection closed */
-        /* Check if timeout occurred */
-        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            /* In case of timeout one can choose to retry calling
-             * httpd_req_recv(), but to keep it simple, here we
-             * respond with an HTTP 408 (Request Timeout) error */
-            httpd_resp_send_408(req);
-        }
-        /* In case of error, returning ESP_FAIL will
-         * ensure that the underlying socket is closed */
-        return ESP_FAIL;
-    }
-
-    /* Send a simple response */
-    const char resp[] = "URI POST Response";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
-    return ESP_OK;
-}
+//esp_err_t post_handler(httpd_req_t *req)
+//{
+//    /* Destination buffer for content of HTTP POST request.
+//     * httpd_req_recv() accepts char* only, but content could
+//     * as well be any binary data (needs type casting).
+//     * In case of string data, null termination will be absent, and
+//     * content length would give length of string */
+//    char content[100];
+//
+//    /* Truncate if content length larger than the buffer */
+//    size_t recv_size = MIN(req->content_len, sizeof(content));
+//
+//    int ret = httpd_req_recv(req, content, recv_size);
+//    if (ret <= 0) {  /* 0 return value indicates connection closed */
+//        /* Check if timeout occurred */
+//        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+//            /* In case of timeout one can choose to retry calling
+//             * httpd_req_recv(), but to keep it simple, here we
+//             * respond with an HTTP 408 (Request Timeout) error */
+//            httpd_resp_send_408(req);
+//        }
+//        /* In case of error, returning ESP_FAIL will
+//         * ensure that the underlying socket is closed */
+//        return ESP_FAIL;
+//    }
+//
+//    /* Send a simple response */
+//    const char resp[] = "URI POST Response";
+//    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+//    return ESP_OK;
+//}
 
 esp_err_t ws_handler(httpd_req_t *req) {
     if (req->method == HTTP_GET) {
@@ -111,19 +111,19 @@ httpd_uri_t uri_ws = {
 
 /* URI handler structure for GET /uri */
 httpd_uri_t uri_get = {
-    .uri      = "/uri",
+    .uri      = "/",
     .method   = HTTP_GET,
     .handler  = get_handler,
     .user_ctx = NULL
 };
 
 /* URI handler structure for POST /uri */
-httpd_uri_t uri_post = {
-    .uri      = "/uri",
-    .method   = HTTP_POST,
-    .handler  = post_handler,
-    .user_ctx = NULL
-};
+//httpd_uri_t uri_post = {
+//    .uri      = "/uri",
+//    .method   = HTTP_POST,
+//    .handler  = post_handler,
+//    .user_ctx = NULL
+//};
 
 /* Function for starting the webserver */
 httpd_handle_t start_http_webserver(void)
@@ -138,7 +138,7 @@ httpd_handle_t start_http_webserver(void)
     if (httpd_start(&server, &config) == ESP_OK) {
         /* Register URI handlers */
         httpd_register_uri_handler(server, &uri_get);
-        httpd_register_uri_handler(server, &uri_post);
+//        httpd_register_uri_handler(server, &uri_post);
         httpd_register_uri_handler(server, &uri_ws);
     }
     /* If server failed to start, handle will be NULL */
